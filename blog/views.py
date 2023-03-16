5550745c24c66f1df179540d1c423103975b2563
navarreteeduardo <navarrete.eduardo@yahoo.com>
2023-03-15 22:07:14 -0500

    acualizar CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
+# blog/views.py
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
+
 
 class BlogListView(ListView):
     model = Post
@@ -9,3 +14,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "blog/post_new.html"
+    fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "blog/post_edit.html"
+    fields = ["title", "body", "author"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "blog/post_delete.html"
+    success_url = reverse_lazy("home")
