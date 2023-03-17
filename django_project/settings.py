4c9fffef49ff0311ac509b6cb60582a01b85dbde
navarreteeduardo <navarrete.eduardo@yahoo.com>
2023-03-16 21:42:28 -0500

    install gunicorn

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)y&o++qz$u5fm*)=+=e1^5!qn5v@73ed%)me%dk)p!9357dn4z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
